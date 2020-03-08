186291aa93ec49f6ce56ed1551feaef5cf77fae0
LeeElena <leeelena90@icloud.com>
2020-03-09 01:05:04 +0900

    js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,51 @@
 var hotelSearch = document.querySelector(".search-btn");
-var popup = document.querySelector(".modal-search");
-var date = popup.querySelector("[name=arrival-date]");
+var popup = document.querySelector(".modal");
+var form = popup.querySelector("form");
+var arrivalDate = popup.querySelector("[name=arrival-date]");
+var departureDate = popup.querySelector("[name=date-of-departure]");
+var adultsNumber = popup.querySelector("[name=adults]");
+var childrenNumber = popup.querySelector("[name=children]");
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  adultsStorage = localStorage.getItem("adultsNumber");
+  childrenStorage = localStorage.getItem("childrenNumber");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 hotelSearch.addEventListener("click", function () {
-  console.log("Клик по кнопке");
-  popup.classList.add("modal-hide");
+  popup.classList.toggle("modal-show");
+  arrivalDate.focus();
+  if (adultsStorage) {
+    adultsNumber.value = adultsStorage;
+  };
+  if (childrenStorage) {
+    childrenNumber.value = childrenStorage;
+  }
 });
 
-hotelSearch.addEventListener("click", function () {
-  console.log("Клик по кнопке");
-  popup.classList.toggle("modal-hide");
+form.addEventListener("submit", function (evt) {
+  if (!arrivalDate.value || !departureDate.value || !adultsNumber.value || !childrenNumber.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("adultsNumber", adultsNumber.value);
+      localStorage.setItem("childrenNumber", childrenNumber.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.add("modal-error");
+    }
+  }
 });
