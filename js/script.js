c6bb020bb923a9313dc261b7515731116df08bd9
LeeElena <leeelena90@icloud.com>
2020-03-21 00:48:58 +0900

    правки

--- js/script.js
+++ js/script.js
@@ -5,25 +5,10 @@ var arrivalDate = popup.querySelector("[name=arrival-date]");
 var departureDate = popup.querySelector("[name=date-of-departure]");
 var adultsNumber = popup.querySelector("[name=adults]");
 var childrenNumber = popup.querySelector("[name=children]");
-var isStorageSupport = true;
-var storage = "";
-
-try {
-  adultsStorage = localStorage.getItem("adultsNumber");
-  childrenStorage = localStorage.getItem("childrenNumber");
-} catch (err) {
-  isStorageSupport = false;
-}
 
 hotelSearch.addEventListener("click", function () {
   popup.classList.toggle("modal-show");
   arrivalDate.focus();
-  if (adultsStorage) {
-    adultsNumber.value = adultsStorage;
-  };
-  if (childrenStorage) {
-    childrenNumber.value = childrenStorage;
-  }
 });
 
 form.addEventListener("submit", function (evt) {
@@ -32,11 +17,6 @@ form.addEventListener("submit", function (evt) {
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("adultsNumber", adultsNumber.value);
-      localStorage.setItem("childrenNumber", childrenNumber.value);
-    }
   }
 });
 
